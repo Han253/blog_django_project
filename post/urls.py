aedaecd3a76c603a158b84b34db662ea91b9118f
programador945 <programadorgato@gmail.com>
2025-03-31 13:19:43 -0500

    add class 3 changes

--- post/urls.py
+++ post/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('publicaciones/', views.publicaciones, name='publicaciones'),
     path('perfil/', views.perfil, name='perfil'),
     path('publicaciones/nueva/', views.nueva_publicacion, name='nueva_publicacion'),
+    path('publicaciones/editar/<int:post_id>/', views.editar_publicacion, name='editar_publicacion'),
+    path('publicaciones/eliminar/<int:post_id>/', views.eliminar_publicacion, name='eliminar_publicacion'),
 ]
--- post/views.py
+++ post/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .forms import PostForm
 from django.contrib.auth.models import User  # Importa el modelo User
 from .models import Post  # Importar modelo post
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -9,24 +10,51 @@ from .models import Post  # Importar modelo post
 def index(request):
     return render(request, 'post/index.html')
 
+@login_required
 def categorias(request):
     return render(request, 'post/categorias.html')
 
+@login_required
 def publicaciones(request):
-    posts = Post.objects.all().order_by('-created_at')  # Ordenados por fecha de creación
+    if request.user.is_superuser or request.user.is_staff:
+        posts = Post.objects.all().order_by('-created_at')  # Muestra todos los posts
+    else:
+        posts = Post.objects.filter(author=request.user).order_by('-created_at')  # Muestra solo los del usuario actual
     return render(request, 'post/publicaciones.html', {'posts': posts})
 
+@login_required
 def perfil(request):
-    return render(request, 'post/perfil_usuario.html')
+    return render(request, 'post/perfil_usuario.html', {'usuario': request.user})
 
+@login_required
 def nueva_publicacion(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)  # No guarda aún en la BD
-            post.author = User.objects.get(id=1)  # Asigna el usuario con ID 1
+            post.author = request.user  # Asigna el usuario autenticado
             post.save()  # Guarda en la BD
             return redirect('publicaciones')  # Redirigir después de guardar
     else:
         form = PostForm()
     return render(request, 'post/nueva_publicacion.html', {'form': form})
+
+@login_required
+def editar_publicacion(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('publicaciones')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post/editar_publicacion.html', {'form': form})
+
+@login_required
+def eliminar_publicacion(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('publicaciones')
+    return render(request, 'post/borrar_publicacion.html', {'post': post})
