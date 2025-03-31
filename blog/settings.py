0d965325e1dc50fbfa1aa11d61a17ec8de51ee4f
programador945 <programadorgato@gmail.com>
2025-03-31 17:42:32 -0500

    add CRUD and LOGIN

--- blog/settings.py
+++ blog/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Redirecciones para login y logout
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = '/login/'
